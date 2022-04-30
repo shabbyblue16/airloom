c9f1ff131b45729f34aed90f82fb20aa05b76c99
J <jbelding16@gmail.com>
2022-04-30 05:43:57 -0400

    Added values component to landing page

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,14 @@ import NavBar from './NavBar';
 import Footer from './Footer';
 import ItemList from './ItemList';
 import HeroLayout from './product/HeroLayout';
+import Hero from './product/Hero';
+import Values from './product/Values';
 
 export {
   NavBar,
   Footer,
   ItemList,
   HeroLayout,
+  Hero,
+  Values,
 };
